b09f0e1d89c6ab32e17c83ccb3659b0a8c5431b8
SkyWasabi <dakeheng@hotmail.com>
2016-03-09 00:34:26 +1100

    Finished passing response from object file to activity file

---
+++ app/src/main/java/com/example/dakeh/emojic8ball/MagicEightBallModel.java
@@ -0,0 +1,52 @@
+package com.example.dakeh.emojic8ball;
+
+import android.util.Log;
+
+import java.util.Objects;
+
+/**
+ * Created by dakeh on 3/7/2016.
+ */
+public class MagicEightBallModel extends Object {
+
+
+    String [] responseArray;
+    static String [] initialResponseArray = {"Ask again later",
+                                    "It is certain",
+                                    "It is decidedly so",
+                                    "Without a doubt",
+                                    "Yes, definitely",
+                                    "You may rely on it",
+                                    "As I see it, yes",
+                                    "Most likely",
+                                    "Outlook good",
+                                    "Yes",
+                                    "Signs point to yes",
+                                    "Reply hazy try again",
+                                    "Better not tell you now",
+                                    "Cannot predict now",
+                                    "Concentrate and ask again",
+                                    "Don't count on it",
+                                    "My reply is no",
+                                    "My sources say no",
+                                    "Outlook not so good",
+                                    "Very doubtful"};
+
+    public MagicEightBallModel(){
+        responseArray = initialResponseArray;
+    }
+
+    public MagicEightBallModel(String [] extraresponse) {
+//        Log.d("array", Integer.toString(extraresponse.length));
+
+        responseArray = new String[initialResponseArray.length + extraresponse.length];
+
+        System.arraycopy(initialResponseArray, 0, responseArray, 0, initialResponseArray.length);
+        System.arraycopy(extraresponse, 0, responseArray, initialResponseArray.length, extraresponse.length);
+
+//        for (int i = 0; i <= responseArray.length; i++)
+//        {
+//            Log.d("Response", responseArray[i]);
+//        }
+    }
+}
--- app/src/main/java/com/example/dakeh/emojic8ball/MainActivity.java
+++ app/src/main/java/com/example/dakeh/emojic8ball/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.dakeh.emojic8ball;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        double age = 21;
+        String name = "Tan Shou Heng";
+        String responseLog;
+
+        String agge = String.format("%.2f", age);
+
+        Log.d("Name", "Tan Shou Heng");
+        Log.d("Age", agge);
+        Log.d("Name in variable", name);
+
+        String [] extraResponseArray = {"Hello World", "Happy go lucky"};
+
+        MagicEightBallModel mb = new MagicEightBallModel(extraResponseArray);
+
+        Log.d("Question 1","Will I get full marks for this lab");
+        responseLog = generateResponse(mb);
+        Log.d("Response 1", responseLog);
+
+        Log.d("Question 2","Will the Cronulla sharks receive a premiership this year");
+        responseLog = generateResponse(mb);
+        Log.d("Response 2", responseLog);
+
+        Log.d("Question 3","Will I end up becoming a cat person when I get old");
+        responseLog = generateResponse(mb);
+        Log.d("Response 3", responseLog);
+
+    }
+
+    public static String generateResponse(MagicEightBallModel mb) {
+//
+//        String test = Integer.toString(mb.responseArray.length);
+//        Log.d("ArraySize", test);
+        Random randomGenerator = new Random();
+        int randomint = randomGenerator.nextInt(mb.responseArray.length);
+        String num = Integer.toString(randomint);
+        Log.d("Number", num);
+
+        return mb.responseArray[randomint];
     }
 }
